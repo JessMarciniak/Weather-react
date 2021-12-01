9bec6755bec5fa0140e8ec7b4b898b88f27e941a
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-01 14:52:32 -0500

    Consolidate files

--- src/weather-react/src/App.js
+++ src/weather-react/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CitySearch from "./CitySearch";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <CitySearch />
+      <br />
+      <small>
+        Coded by Jessica Marciniak{" "}
+        <a href="https://github.com/JessMarciniak/Weather-react">GitHub</a>
+      </small>
     </div>
   );
 }
-
-export default App;
