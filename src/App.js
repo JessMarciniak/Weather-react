ff9be1365e73da52576f698d3b9461aa0cc119e9
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-01 13:33:12 -0500

    Imported files from sandbox

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CitySearch from "./CitySearch";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <CitySearch />
+    </div>
+  );
+}
