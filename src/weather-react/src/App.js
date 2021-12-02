6b0d3d9da1990c96a338fbf8f8d7d125cc490f55
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-02 10:51:13 -0500

    Rearrange files

--- src/weather-react/src/App.js
+++ src/weather-react/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import CitySearch from "./CitySearch";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <CitySearch />
-      <br />
-      <small>
-        Coded by Jessica Marciniak{" "}
-        <a href="https://github.com/JessMarciniak/Weather-react">GitHub</a>
-      </small>
+      <div className="Header">
+        <h1>Weather App</h1>
+      </div>
+      <div className="container">
+        <i class="fas fa-umbrella"></i>
+        <CitySearch />
+      </div>
+      <div className="Footer">
+        <small>
+          Coded by Jessica Marciniak{" "}
+          <a href="https://github.com/JessMarciniak/Weather-react">GitHub</a>
+        </small>
+      </div>
     </div>
   );
 }
